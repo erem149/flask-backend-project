880126b7a3dbfdb4ea82990e9f3a96439bd42998
erem149 <ryanmaulana149@gmail.com>
2024-01-07 00:00:13 +0700

    Updated routes.py file

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,10 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello people with the spirit of learning!~ "
+    return render_template('index.html')
+    # return "Hello people with the spirit of learning!~ "
+# # Try to see the difference between the two lines below
+# return render_template('index.html')
