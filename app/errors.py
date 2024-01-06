08f02938d08c91d5848e9a6d2f953c64fc367c68
erem149 <ryanmaulana149@gmail.com>
2024-01-07 00:13:08 +0700

    Updated errors.py file

--- app/errors.py
+++ app/errors.py
@@ -1,6 +1,6 @@
-from app import app, db
+from app import app
 from flask import render_template
 
-@app.error_handler(404)
+@app.errorhandler(404)
 def not_found(error):
     return render_template('404.html'), 404
