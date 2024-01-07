56fe6a4e76e5884387f4fcbda8b70ae46b2b0615
erem149 <ryanmaulana149@gmail.com>
2024-01-07 15:27:43 +0700

    Updated __init__.py file

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,36 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import logging, os
+from logging.handlers import RotatingFileHandler
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
 bootstrap = Bootstrap(app)
 
-from app import routes, errors
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+if not app.debug:
+    if app.config['LOG_TO_STDOUT']:
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(logging.INFO)
+        app.logger.addHandler(stream_handler)
+    else:
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/uploads.log', maxBytes=10240, backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(astime)s %(levelname)s: %(message)s '
+            '[in %(pathname)s:%(lineno)d]'
+        ))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+    
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Handling File Uploads')
+
+from app import routes, errors, models
