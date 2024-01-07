f4f6a3261c217571c356043f84f0e8b76083ab65
erem149 <ryanmaulana149@gmail.com>
2024-01-07 15:31:01 +0700

    Updated routes.py file

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,50 @@
-from app import app
-from flask import render_template
+from app import app, db
+from flask import flash, redirect, render_template, url_for
+from app.forms import UserForm
+from app.models import User
+from werkzeug.utils import secure_filename
+import os
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html')
+# @app.route('/')
+# @app.route('/index')
+# def index():
+#     return render_template('index.html')
 # # Try to see the difference between the two lines below
 # return render_template('index.html')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+    users = User.query.all()
+    form = UserForm()
+    if form.validate_on_submit():
+        # User object
+        admin = User(
+            username = form.username.data,
+            email = form.email.data,
+            about_me = form.about_me.data
+        )
+
+        # Handling file upload
+        uploaded_file = form.avatar.data
+        filename = secure_filename(uploaded_file.filename)
+        avatar_path = os.path.join(app.config['UPLOAD_PATH'], filename)
+        uploaded_file.save(avatar_path)
+        admin.avatar = avatar_path
+        path_list = admin.avatar.split('/')[1:]
+        new_path = '/'.join(path_list)
+
+        # Update the database
+        admin.avatar = new_path
+        db.session.add(admin)
+        db.session.commit()
+
+        # UX
+        flash('Your changes have been saved.')
+
+        return redirect(url_for('index'))
+    return render_template(
+            'index.html',
+            form=form,
+            title='User Form',
+            users=users
+            )
